9637e745e866f5f8af435db2b32ad5116193d149
AadhavanPL <aadhavanlenin@gmail.com>
2023-02-03 20:10:42 +0530

    initial files

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
-import '@/styles/globals.css'
+import '../styles/globals.css'
+import '../styles/variables.css'
+import '../styles/media-queries.css'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+	return <Component {...pageProps} />
 }
+
+export default MyApp
