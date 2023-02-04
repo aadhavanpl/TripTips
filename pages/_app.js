3581dad4d3599ddc2ffa3b005f3c2d98464a9fdd
AadhavanPL <aadhavanlenin@gmail.com>
2023-02-04 15:37:13 +0530

    auth done

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,7 @@
+import { AnimatePresence, motion } from 'framer-motion'
+import { GlobalContextWrapper } from 'lib/global-context'
+
+//CSS
 import '../styles/globals.css'
 import '../styles/variables.css'
 import '../styles/media-queries.css'
@@ -5,8 +9,28 @@ import '../styles/media-queries.css'
 //Fonts
 import '../assets/fonts/Poppins/stylesheet.css'
 
+import 'firebase-config'
+
+import { useRouter } from 'next/router'
+import Head from 'next/head'
+
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />
+	const router = useRouter()
+
+	return (
+		<>
+			<Head>
+				<title>TripTips</title>
+			</Head>
+			<GlobalContextWrapper>
+				<AnimatePresence>
+					<motion.div key={router.pathname} initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+						<Component {...pageProps} />
+					</motion.div>
+				</AnimatePresence>
+			</GlobalContextWrapper>
+		</>
+	)
 }
 
 export default MyApp
